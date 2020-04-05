56aaf0ce170c925d99f27010e73fcb2b4e32389e
ShaneHolmes <shaneholmes@qq.com>
2020-04-05 22:06:54 +0800

    HBase MapReduce

---
+++ src/main/java/com/shane/hbase/HBaseMRFruit2FruitRunner.java
@@ -0,0 +1,57 @@
+package com.shane.hbase;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.IOException;
+
+public class HBaseMRFruit2FruitRunner extends Configured implements Tool {
+
+    public int run(String[] strings) throws Exception {
+        //获得配置
+        Configuration conf = this.getConf();
+        //创建任务,并配置
+        Job job = Job.getInstance(conf,this.getClass().getSimpleName());
+        job.setJarByClass(HBaseMRFruit2FruitRunner.class);
+        //配置 Job
+        Scan scan = new Scan();
+        scan.setCacheBlocks(false);
+        scan.setCaching(500);
+        //设置 Mapper，注意导入的是 mapreduce 包下的，不是 mapred 包下的，后者是老版本
+        TableMapReduceUtil.initTableMapperJob(
+                "fruit", //数据源的表名
+                scan, //scan 扫描控制器
+                HBaseMRReadFruitMapper.class,//设置 Mapper 类
+                ImmutableBytesWritable.class,//设置 Mapper 输出 key 类型
+                Put.class,//设置 Mapper 输出 value 值类型
+                job//设置给哪个 JOB
+        );
+        //设置 Reducer
+        TableMapReduceUtil.initTableReducerJob("fruit_mr",
+                HBaseMRWriteFruitReducer.class, job);
+        //设置 Reduce 数量，最少 1 个
+        job.setNumReduceTasks(1);
+        boolean isSuccess = job.waitForCompletion(true);
+        if(!isSuccess){
+
+            throw new IOException("Job running with error");
+        }
+        return isSuccess ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = HBaseConfiguration.create();
+        int status = ToolRunner.run(conf, new HBaseMRFruit2FruitRunner(), args);
+        System.exit(status);
+
+    }
+
+}
